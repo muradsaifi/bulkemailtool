34b3f75e001a8b63ded0d498ce4e1b735b77346a
root <root@igp.igphost.xyz>
2022-03-09 09:00:45 +0100

    modified:   src/routes/web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,7 +3,7 @@
 use Muradsaifi\Bulkemailtool\Controllers\BulkController;
 
 
-// Route::post('bulk_email_tool', [BulkController::class, 'submit'])->name('bulk_email_tool.submit');
+Route::post('bulk_email_tool', [BulkController::class, 'submit'])->name('bulk_email_tool.submit');
 
-// Route::get('bulk_email_tool', [BulkController::class, 'send'])->name('bulk_email_tool.send');
-// Route::get('bulk_email_recent', [BulkController::class, 'bulk_email_recent'])->name('bulk_email_recent');
+Route::get('bulk_email_tool', [BulkController::class, 'send'])->name('bulk_email_tool.send');
+Route::get('bulk_email_recent', [BulkController::class, 'bulk_email_recent'])->name('bulk_email_recent');
